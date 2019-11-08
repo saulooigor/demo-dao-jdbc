892d2d79b52698e9166eec15b8cf2dea431839e3
Saulo Igor Silva Alves do Carmo <sauloalves@frwk.com.br>
2019-11-08 18:05:49 -0300

    Alter Seller Class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -77,4 +77,36 @@ public class Seller implements Serializable{
 		this.department = department;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+
+	
 }
